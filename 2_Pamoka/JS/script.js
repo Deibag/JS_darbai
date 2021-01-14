deacfe1a5b51fc95a8668209196555fa41bfccc4
Deividas B <deividasbagdonas34@gmail.com>
2021-01-14 18:19:18 +0200

    Pabaigtos uzduotys

--- 2_Pamoka/JS/script.js
+++ 2_Pamoka/JS/script.js
@@ -40,7 +40,7 @@ sumForm.addEventListener("submit", function(e){
 
 //Exercise 3
 var kmiForm = document.querySelector("#kmiForm");
-var result = document.getElementById("kmi");
+var kmiresult = document.getElementById("kmi");
 
 kmiForm.addEventListener("submit", function(e){
     e.preventDefault();
@@ -48,6 +48,54 @@ kmiForm.addEventListener("submit", function(e){
     var weight = kmiForm["weight"].value;
     var answer = Math.floor(parseInt(weight) / Math.pow(parseInt(height)/100, 2));
 
-    result.innerHTML = "Jusu KMI yra " + answer;
+    kmiresult.innerHTML = "Jusu KMI yra " + answer;
 })
 
+
+//Exercise 4
+var taskForm = document.querySelector("#taskForm");
+var result = document.querySelector("#list")
+
+taskForm.addEventListener("submit", function(e){
+    e.preventDefault();
+    var toAdd = taskForm["task"].value;
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(toAdd));
+    result.appendChild(li);
+})
+
+
+//Exercise 6
+var div = document.querySelector("#block");
+div.style.backgroundSize = "1000px 400px"
+function show(e){
+    e.preventDefault();
+    var selector = document.querySelector("#id_of_select");
+    var value = selector[selector.selectedIndex].text;
+    switch(value){
+        case "Pavasaris":
+            div.style.backgroundImage = "url(./IMG/pavasaris.jpg)";
+            break;
+        case "Vasara":
+            div.style.backgroundImage = "url(./IMG/vasara.jpg)";;
+            break;
+        case "Ruduo":
+            div.style.backgroundImage = "url(./IMG/ruduo.jpg)";;
+            break;
+        case "Žiema":
+            div.style.backgroundImage = "url(./IMG/ziema.jpg)";;
+            break;
+    }
+}
+
+document.getElementById('show').addEventListener("click", show);
+
+
+//Exercise 7
+function change(img){
+    var top = document.querySelector('#top');
+    var text = document.querySelector('#ctext');
+    console.log(top);
+    top.src = document.getElementById(img).src;
+    text.innerHTML = document.getElementById(img).alt;
+}
